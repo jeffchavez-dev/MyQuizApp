b55c2a30bfb9369e56565ba78a08c56046cfb42b
Jeff Chavez <jeff.chavez0828@gmail.com>
2022-08-26 13:33:27 +0800

    Updated radio input IDs

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // create quiz data array
 // declare variables needed: title header, start quiz, quiz-body, question-title, answers
-// loadQuizData
-// Submit button to increment current quuestion and 
+// Create a function to load the QuizData to the Quiz body
+// Submit button to increment questions and answers
+// Create a function to keep track of the current selection  
 // check the selection and get total score
 
 
@@ -41,22 +42,19 @@ const quizBody = document.getElementById("quiz-body");
 const startQuiz = document.getElementById("start-quiz");
 const restartQuiz = document.getElementById("restart-quiz");
 const questionTitle = document.getElementById("question-title");
-const a_text = document.getElementById("a");
-const b_text = document.getElementById("b");
-const c_text = document.getElementById("c");
-const answers = document.querySelectorAll(".answer")
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const answerEls = document.querySelectorAll(".answer");
+const submitButton = document.querySelector(".submit-btn");
 
 // loadQuizData
 
 let currentQuestion = 0;
-
-const submitButton = document.querySelector(".submit-btn")
-
-
+let score = 0;
 
 const loadQuizData = () => {
     const quiz = quizData[currentQuestion];
-
     questionTitle.innerText = quiz.question;
     a_text.innerText = quiz.a;
     b_text.innerText = quiz.b;
@@ -65,26 +63,50 @@ const loadQuizData = () => {
 
 loadQuizData();
 
+
+const getSelected = () => {
+
+    let answer; // no assignment yet
+
+    // loop over the answers 
+
+    answerEls.forEach((answerEl) => {
+        if(answerEl.checked) {
+            answer = answerEl.id
+        }
+    })
+    return answer; // it will return the ID of the selection
+}
+
+
+
+
+
 // Submit button to increment current question  
 submitButton.addEventListener('click', () => {
+    // once the button is clicked check also if the selection matched 
+    // the correct answer as indicated in quizdata
+   
+    const answer = getSelected();
+
+
+    // if(answer) { //check first to see if there is an answer
+
+    // }
+    if(answer === quizData[currentQuestion].correct) {
+        score++;
+    }
     currentQuestion++;
+
+    // prevent loadQuizData from incrementing if no question remains
+    if()
+    
     loadQuizData();
-    getSelection()
+    
 })
 
 // check the selection and get total scores
 
-function getSelection() {
-    // let answer;
-    answers.forEach(answer => {
-        if(answer.checked === quizData[currentQuestion].correct) {
-            console.log("answer selected");
-        } else {
-            console.log("no answer selected");
-        }
-    });
-
-}
 
 
 restartQuiz.addEventListener('click', () => {
